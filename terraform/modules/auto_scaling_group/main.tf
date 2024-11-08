0802bae07281bc3ff34c1226e2ac0eb1676869a4
Janam Kahtiwada <jhapali.janam@gmail.com>
2024-11-08 20:48:01 +0545

    updated launch_template to do a rolling update so hA is maintained

--- terraform/modules/auto_scaling_group/main.tf
+++ terraform/modules/auto_scaling_group/main.tf
@@ -3,6 +3,7 @@ resource "aws_autoscaling_group" "web_asg" {
   max_size             = var.max_size
   min_size             = var.min_size
   vpc_zone_identifier  = var.subnet_ids
+  
   launch_template {
     id      = var.launch_template_id
     version = "$Latest"
@@ -11,6 +12,16 @@ resource "aws_autoscaling_group" "web_asg" {
   health_check_type         = "ELB"
   health_check_grace_period = 300
 
+  instance_refresh {
+    strategy = "Rolling"
+  
+    preferences {
+      min_healthy_percentage = 50
+      instance_warmup        = 300  # Waits 5 minutes before checking health
+    }
+  
+    triggers = ["launch_template"]
+  }
   tag {
     key                 = "Name"
     value               = "Laravel Prod Application Server"
